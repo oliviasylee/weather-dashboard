2f25a118abaebe90c8300b28393e8a06620bd1f2
olivialee <oliviasylee@gmail.com>
2022-12-14 17:45:35 -0500

    Added form submit handler and get api in js file

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,23 +1,57 @@
 var weatherFormEl = document.querySelector('#weather-form');
 var cityNameInputEl = document.querySelector('#cityname');
 var weatherContainerEl = document.querySelector('#weather-container');
+var weatherSearchTerm = document.querySelector('#weather-search-term');
 const key="1362316676e7462ce8cb9c952d85a52f"
-//var weatherSearchTerm = document.querySelector('#weather-search-term');
+
 //const apiUrl='https://api.openweathermap.org/data/2.5/weather?q=' + city+ '&appid=' + key';
 
-function getCityWeather(city) {
-  fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city+ '&appid=' + key+'&cnt=5')
-  .then(function(resp) {
-      return resp.json() 
-  })
-  .then(function(data) {
-      console.log('--->'+(JSON.stringify(data)));
-      drawWeather(data);
-  })
-  .catch(function() {
-      // catch any errors
-  });
-}
+let formSubmitHandler = function (event) {
+  event.preventDefault();
+
+  let cityName = cityNameInputEl.value.trim();
+
+  if (cityName) {
+    getCityWeather(cityName);
+
+    weatherContainerEl.textContent = '';
+    cityNameInputEl.value = '';
+  } else {
+    alert('Please enter a city!');
+  }
+};
+
+let getCityWeather = function (city) {
+  let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city+ "&limit=5"+"&appid=" + key;
+  console.log(apiUrl)
+
+  fetch(apiUrl)
+    .then(function (response){
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function (data) {
+          console.log(data);
+
+        });
+      }}
+    )};
+
+// let displayWeather = function ()
+
+// function getCityWeather(city) {
+//   fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city+ '&limit=5'+'&appid=' + key)
+//   console.log(recipeUrl)
+//   .then(function(resp) {
+//       return resp.json() 
+//   })
+//   .then(function(data) {
+//       console.log('--->'+(JSON.stringify(data)));
+//       drawWeather(data);
+//   })
+//   .catch(function() {
+//       // catch any errors
+//   });
+// }
 
 
 //Event Listeners on button click
@@ -31,6 +65,8 @@ document.addEventListener("DOMContentLoaded", () => {
       }       
  }) 
 });
+
+weatherFormEl.addEventListener('submit', formSubmitHandler);
 
 // var getCityWeather = function (city) {
 //   var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city+ '&appid=' + key+'';
