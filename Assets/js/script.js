4d7fd7b10d3900e00dd783d1d014c5946a7ddea5
olivialee <oliviasylee@gmail.com>
2022-12-23 10:14:32 -0500

    Create 5-day forecast

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,47 +1,27 @@
-let weatherFormEl = document.querySelector('#weather-form');
-let cityNameInputEl = document.querySelector('#cityname');
-let weatherContainerEl = document.querySelector('#weather-container');
-let weatherSearchCity = document.querySelector('#weather-search-city');
-let descriptionContainer = document.querySelector(".description");
+var weatherFormEl = document.querySelector('#weather-form');
+var cityNameInputEl = document.querySelector('#cityname');
+var descriptionContainer = document.querySelector(".description");
+var currentWeatherEl = document.getElementById("currentWeather");
+var fivedayEl = document.getElementById("fivedayWeather");
 
 const key="1362316676e7462ce8cb9c952d85a52f"
 
-let formSubmitHandler = function (event) {
+var formSubmitHandler = function (event) {
   event.preventDefault();
 
-  let cityName = cityNameInputEl.value.trim();
+  var cityName = cityNameInputEl.value.trim();
 
   if (cityName) {
     getCityWeather(cityName);
 
-    weatherContainerEl.textContent = '';
     cityNameInputEl.value = '';
   } else {
     alert('Please enter a city!');
   }
 };
 
-// //Event Listeners on button click
-// document.addEventListener("DOMContentLoaded", () => {
-//   // Handling button click
-//   document.querySelector(".btn").addEventListener("click", () => {
-//       const searchedCity = document.querySelector('.form-input');
-//       console.log(searchedCity.value);
-//       if(searchedCity.value){
-//         getCityWeather(searchedCity.value);
-//       }       
-//  }) 
-// });
-
-// api로 불러온 데이터를 weather-container에 담기
-// let displayWeather = function (searchTerm) {
-//   weatherSearchCity.textContent = searchTerm;
-//   $("#weather-search-city").html(data.name);
-//   console.log(data.name);
-// }
-
-let getCityWeather = function (city) {
-  let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city+ "&limit=5"+"&appid=" + key;
+function getCityWeather(city) {
+  var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city+ "&limit=5"+"&appid=" + key;
   console.log(apiUrl)
 
   fetch(apiUrl)
@@ -52,73 +32,100 @@ let getCityWeather = function (city) {
           console.log(data);
           console.log(data.name);
 
-          // data.name을 weather-search-city에 표시되게 하고 싶음
-          weatherSearchCity.innerText = data.name;
+          currentWeatherEl.classList.remove("d-none");
 
           // City name in card-body
-          let cityTitle = document.querySelector("#cityTitle");
+          var cityTitle = document.querySelector("#cityTitle");
           cityTitle.innerText = data.name;
           
           // Date + Icon
-          const dt = data.dt;
-          let today = new Date(dt*1000);
-          console.log(today.toDateString());
-          cityTitle.append(today.toDateString());
-          
-          let iconUrl = "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
+          var todayDate = dayjs().format("dddd, MMMM D YYYY")
+          cityTitle.innerHTML = data.name + " (" + todayDate + ") ";
+        //  Another way to display Date 
+        //  const dt = data.dt;
+        //  var today = new Date(dt*1000);
+        //  console.log(today.toDateString());
+        //  cityTitle.append(today.toDateString());
+        //  cityTitle.innerHTML = data.name + " (" + today.toDateString() + ") ";
+
+          var iconUrl = "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
           console.log(data.weather[0].icon);
           console.log(iconUrl);
           
-          // icon은 object, {icon: '02n'}, object inside array
-          // https://stackoverflow.com/questions/50822205/accessing-object-inside-array
-          // 아이콘 주소를 html의 classname todayIcon에 붙여서 출력
-          // 아 src값이 없기 때문에 null이 나오는구나 현재 src는 ... empty상태라?
-          // 그럼 태그를 찾아서 거기에 iconurl만 넣어주기로 하자 -> 계속 null값이 나옴
-          // 내 생각에 html에서 img src가 값이 없는 상태이기 때문에 그걸 불러와서 넣는다는 개념이 틀린 듯
-          // 그래서 대안으로 아예 js로 img태그를 만들어서 url을 생성 후 타이틀 옆에 붙임
-
-          let todayIcon = document.createElement('img');
+          // icon is an object, {icon: '02n'}, object inside array
+          var todayIcon = document.createElement('img');
           todayIcon.src = iconUrl;
           document.getElementById('cityTitle').appendChild(todayIcon);
           
           // Temp
-          // need to add subtitle "Temp:" 
-          // need to add for loop and p element?
-          let descrList = document.createElement('p');
-          descriptionContainer.appendChild(descrList)
-      
-        //   for (let i=0; i< data.hits[random].recipe.ingredientLines.length; i++) {
-        //     var item = $("<li>")
-        //     item.html(data.hits[random].recipe.ingredientLines[i]);
-        //     list.append(item)
-        // }
-
-          // need a for loop -> 5days forecast
-          let todayTemp = document.createElement('p');
-          var todayTempNode = document.createTextNode(data.main.temp);
-          console.log(todayTemp);
-          todayTemp.appendChild(todayTempNode);
-          descriptionContainer.appendChild(todayTemp);
-          
-          //todayTemp.html("<h5>Temp:</h5>") + data.main.temp;
+          var idTemp = document.querySelector("#temperature");
+          idTemp.innerHTML = "Temp: " + ((data.main.temp - 273.15) * 1.8 + 32).toFixed(1) + " &#176F";
           
           // Wind
-          // add wind speed under temp
-          let todayWind = document.createElement('p');
-          var todayWindNode = document.createTextNode(data.wind.speed);
-          todayWind.appendChild(todayWindNode);
-          descriptionContainer.appendChild(todayWind);
+          var idWind = document.querySelector("#wind-speed");
+          idWind.innerHTML = "Wind: " + data.wind.speed + " MPH";
           console.log(data.wind.speed);
           
-          // Humidity: add humidity under '
-          let todayHum = document.createElement('p');
-          var todayHumNode = document.createTextNode(data.main.humidity);
-          todayHum.appendChild(todayHumNode);
-          descriptionContainer.appendChild(todayHum);
+          // Humidity
+          var idHum = document.querySelector("#humidity");
+          idHum.innerHTML = "Humidity: " + data.main.humidity + "%";
           console.log(data.main.humidity);
+        })
+      }})
+      
+    // Parse response to display forecast for next 5 days
+      var fiveDaysUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city+"&appid=" + key +"&cnt=5";
+      console.log(fiveDaysUrl)
+  
+    fetch(fiveDaysUrl)
+      .then(function (response){
+        if (response.ok) {
+          console.log(response);
+          response.json().then(function (data) {
+            console.log(data);
+        
+            fivedayEl.classList.remove("d-none");
+        
+    // loop for 5-day forecast Date + icon, Temp, Wind, Humidity
+        const forecastEls = document.querySelectorAll(".forecast");
+        
+    for (i = 0; i < forecastEls.length; i++) {
+    forecastEls[i].innerHTML = "";
 
-        });
-      }}
-    )};
+    var iconUrl = "https://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + "@2x.png";
+          var forecastWeatherEl = document.createElement('div');
+
+          // 5-day Date
+          const forecastDate = dayjs().add(i+1, 'day').format("dddd, MMMM D YYYY")
+          var forecastDateDisplay = document.createElement('h5');
+          forecastDateDisplay.textContent = forecastDate;
+          forecastWeatherEl.append(forecastDateDisplay);
+
+          // Icon
+          var forecastIcon = document.createElement('img');
+          forecastIcon.src = iconUrl;
+          forecastWeatherEl.append(forecastIcon);
+
+          // Temp
+          var forecastTemp = document.createElement("p");
+          forecastTemp.innerHTML = "Temp: " + ((data.list[i].main.temp - 273.15) * 1.8 + 32).toFixed(1) + " &#176F";
+          forecastWeatherEl.append(forecastTemp);
+
+          // Wind
+          var forecastWind = document.createElement("p");
+          forecastWind.innerHTML = "Wind: " + data.list[i].wind.speed + " MPH";
+          forecastWeatherEl.append(forecastWind);
+
+          // Humidity
+          var forecastHum = document.createElement("p");
+          forecastHum.innerHTML = "Humidity: " + data.list[i].main.humidity + "%";
+          forecastWeatherEl.append(forecastHum);
+
+          forecastEls[i].append(forecastWeatherEl);
+    }
+        })    
+        }
+        })
+  };
 
 weatherFormEl.addEventListener('submit', formSubmitHandler);
